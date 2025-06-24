60d9e4572f1018d3f21b6afa06796c453459214c
TradeX-app <my.github@tutanota.com>
2025-06-24 20:33:42 +0200

    added style.css to build

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,7 @@
 vite build &&
 cp ./types/calex.d.ts ./dist &&
 cp ./src/i18n/languages.js ./dist &&
+cp ./src/style.css ./dist &&
 cp ./README.md ./dist &&
 cp ./LICENSE ./dist &&
 cp ./CONTRIBUTING.md ./dist &&
